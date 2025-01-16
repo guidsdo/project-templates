de96689d8cf23c2a0b2fc3d093ca618e753310e6
guidsdo <guidsdo@users.noreply.github.com>
2025-01-16 14:23:31 +0100

    Fix monaco webworker config

--- monaco-editor/vite.config.ts
+++ monaco-editor/vite.config.ts
@@ -1,9 +1,25 @@
+import pluginTypescript from "@rollup/plugin-typescript";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        pluginTypescript({
+            tsconfig: "tsconfig.json",
+            sourceMap: true,
+            inlineSources: true
+        })
+    ],
+    worker: {
+        plugins: () => [
+            pluginTypescript({
+                tsconfig: "tsconfig.json",
+                sourceMap: true,
+                inlineSources: true
+            })
+        ]
+    },
     esbuild: { keepNames: true },
     build: { minify: false }
 });
